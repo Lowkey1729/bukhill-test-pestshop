d29ffdf22481475e09596fe30b0648259f084891
Olarewaju Mojeed Olalekan <53194190+Lowkey1729@users.noreply.github.com>
2024-06-30 10:28:15 +0100

    L5-swagger setup

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,19 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *    title="Petshop API documentation",
+ *    version="1.0.0",
+ * )
+ *
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     securityScheme="bearerToken",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
+ * )
+ */
 abstract class Controller
 {
     //
