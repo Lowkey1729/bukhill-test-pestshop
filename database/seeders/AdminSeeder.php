1532c250c0940e63ab076f00e8115466dbd2e68f
Olarewaju Mojeed Olalekan <53194190+Lowkey1729@users.noreply.github.com>
2024-07-06 20:08:19 +0100

    Admin and user data seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Support\Enums\UserType;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::query()->create([
+            'first_name' => 'admin',
+            'last_name' => 'admin',
+            'email' => 'admin@buckhill.co.uk',
+            'password' => bcrypt('admin'),
+            'email_verified_at' => now(),
+            'avatar' => fake()->uuid,
+            'is_marketing' => 0,
+            'type' => UserType::ADMIN->value,
+            'phone_number' => fake()->phoneNumber(),
+            'address' => fake()->address(),
+        ]);
+    }
+}
